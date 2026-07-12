c8e8f565bca0ecced97805ccf8bf5a79caca512e
Raj <149751988+Raj7753@users.noreply.github.com>
2026-07-12 21:58:13 +0530

    Add: binary-tree-diameter - submission-3

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-3.cpp
@@ -0,0 +1,18 @@
+ class Solution {
+public:
+    int result = 0;
+
+    int function(TreeNode* root){
+        if(root == NULL) return 0;
+        int left = function(root -> left);
+        int right = function(root -> right);
+
+         result = max(result, left+right);  
+
+        return max(left,right) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        function (root);
+        return result;
+    }
+};
